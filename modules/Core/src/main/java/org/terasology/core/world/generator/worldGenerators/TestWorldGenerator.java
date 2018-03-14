ce7459e2c7781cb2671d83a016b42b1e2adc7e23
Esa-Petri Tirkkonen <esereja@yahoo.co.uk>
2018-03-15 00:24:17 +0200

    Auto stash before merge of "master" and "github/master"

---
+++ modules/Core/src/main/java/org/terasology/core/world/generator/worldGenerators/TestWorldGenerator.java
@@ -0,0 +1,59 @@
+ 
+/*
+ * Copyright 2013 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.core.world.generator.worldGenerators;
+
+import org.terasology.core.world.generator.facetProviders.BiomeProvider;
+import org.terasology.core.world.generator.facetProviders.FlatSurfaceHeightProvider;
+import org.terasology.core.world.generator.facetProviders.PerlinHumidityProvider;
+import org.terasology.core.world.generator.facetProviders.PerlinSurfaceTemperatureProvider;
+import org.terasology.core.world.generator.facetProviders.SeaLevelProvider;
+import org.terasology.core.world.generator.facetProviders.SurfaceToDensityProvider;
+import org.terasology.core.world.generator.rasterizers.SolidRasterizer;
+import org.terasology.engine.SimpleUri;
+import org.terasology.registry.In;
+import org.terasology.world.generation.BaseFacetedWorldGenerator;
+import org.terasology.world.generation.WorldBuilder;
+import org.terasology.world.generator.RegisterWorldGenerator;
+import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
+
+/**
+ *
+ */
+@RegisterWorldGenerator(id = "testworldgenerator", displayName = "TestWorldGenerator", description = "test flat world generator")
+public class TestWorldGenerator extends BaseFacetedWorldGenerator {
+
+    @In
+    private WorldGeneratorPluginLibrary worldGeneratorPluginLibrary;
+
+    public TestWorldGenerator(SimpleUri uri) {
+        super(uri);
+    }
+
+    @Override
+    protected WorldBuilder createWorld() {
+        return new WorldBuilder(worldGeneratorPluginLibrary)
+                .addProvider(new SeaLevelProvider(32))
+                        // height of 40 so that it is far enough from sea level so that it doesnt just create beachfront
+                .addProvider(new FlatSurfaceHeightProvider(40))
+                .addProvider(new PerlinHumidityProvider())
+                .addProvider(new PerlinSurfaceTemperatureProvider())
+                .addProvider(new BiomeProvider())
+                .addProvider(new SurfaceToDensityProvider())
+                .addRasterizer(new SolidRasterizer())
+                .addPlugins();
+    }
+}
